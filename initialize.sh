50e3f0359a66c1f5dd41eed7fa28b18f728c4132
Lin <ryan.linlan@gmail.com>
2017-07-22 20:40:25 +0800

    update

--- initialize.sh
+++ initialize.sh
@@ -6,5 +6,11 @@ echo "alias proxy_on=\"${AUTOPROXY_PATH}/script.sh on\"" >> ${HOME}/.bashrc
 echo "alias proxy_off=\"${AUTOPROXY_PATH}/script.sh off\"" >> ${HOME}/.bashrc
 echo "alias proxy_list=\"${AUTOPROXY_PATH}/script.sh list\"" >> ${HOME}/.bashrc
 
+ssh -t root@localhost  ' /bin/bash -i -c "
+  '"echo 'alias proxy_on=\\\"${AUTOPROXY_PATH}/script.sh on\\\"'"' > ~/.bash_aliases
+  '"echo 'alias proxy_off=\\\"${AUTOPROXY_PATH}/script.sh off\\\"'"' >> ~/.bash_aliases
+  '"echo 'alias proxy_list=\\\"${AUTOPROXY_PATH}/script.sh list\\\"'"' >> ~/.bash_aliases
+"'
+
 sudo cp cert/CA.crt /usr/local/share/ca-certificates
 sudo update-ca-certificates
